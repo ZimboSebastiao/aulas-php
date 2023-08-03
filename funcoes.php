3952bfd7eeadff5a8e26b6d1b7506f7d45b32665
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-08-03 08:47:11 -0300

    Funções: sub-rotina/procedimento

---
+++ funcoes.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funções no PHP</title>
+</head>
+<body>
+
+    <h1>Criando e usando funções</h1>
+    <hr>
+
+    <h2>Função como procedimento ou sub-rotina</h2>
+
+    <?php 
+    
+        function dadosAutor(){
+            echo "<div>";
+            echo "<p>Tiago B. dos Santos</p>";
+            echo "<p><a href=''>email@aa.com</a></p>";
+            echo "<div/>";
+        }
+    
+    ?>
+
+    <section>
+        <h3>Chamada da sub-rotina/procedimento</h3>
+        <?=dadosAutor()?>
+    </section>
+    
+</body>
+</html>
