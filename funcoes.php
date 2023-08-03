7fc232ed192bcbf32e473a9d9603c16722fd1714
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-08-03 10:07:48 -0300

    Funções: indução de tipos de dados

--- funcoes.php
+++ funcoes.php
@@ -101,7 +101,35 @@
     <p> <?=saudacao("boa tarde")?> </p>
 
 
+    <hr>
 
+    <h2>Indução de tipos de dados</h2>
+
+    <?php 
+
+        /* Tipos de dados mais comuns
+        string -> caracteres em geral
+        int -> números inteiros
+        float -> números com casas decimais
+        array -> vetor
+        */
+
+        /* Indução de tipos deve ser feita nos parâmetros e no retorno da função.
+
+        Isso auxiliará na entrada/saída correta dos dados manipulados pela função, além de melhorar as mensagens de erro quando ocorrem problemas.
+        */
+        function verificaNegativo(int $valor):string {
+            if ($valor < 0) {
+                return "é negativo";
+            } else {
+                return "não é negativo";
+            }
+        }
+
+    ?>
+    
+    <p>Número 10: <?=verificaNegativo(10)?></p>
+    <p>Número -10: <?=verificaNegativo(-10)?></p>
 
 </body>
 </html>
