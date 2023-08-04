fef5a05bd154de73c65ede31519060291219103a
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-08-04 08:43:51 -0300

    adicionando uma tabela

--- exercicios/exercicio05.php
+++ exercicios/exercicio05.php
@@ -15,7 +15,57 @@
             color: green;
             font-weight: bold;
         }
-        p, h1 {text-align: center;}
+        h1 {text-align: center;}
+
+        table, th, td {
+            border: solid 8px black;
+            border-collapse: collapse;
+        }
+
+        table {
+            margin: auto;
+            width: 80%;
+            max-width: 600px;
+        }
+
+        caption {
+            font-weight: bold;
+            text-transform: uppercase;
+            font-size: 1.2rem;
+            padding-bottom: 0.5rem;
+        }
+
+        th {
+            background-color: black;
+            color: white;
+            padding: 1.7rem;
+            font-size: 1.7rem;
+            font-weight: bold;
+        }
+
+
+        td {
+            padding: 10px;
+            font-size: 1.3rem;
+        }
+
+        td:first-child { text-transform: uppercase;}
+
+        td:nth-child(2) { text-align: center;}
+
+        td:last-child {
+            font-size: bold;
+            text-align: right;
+        }
+
+        
+        tr { background-color: lightskyblue;}
+
+        
+        tr:hover, tr:focus {
+            cursor: pointer;
+            background-color: aquamarine;
+        }
         
     </style>
 </head>
@@ -25,13 +75,13 @@
     <?php 
        
         // Função 1: Calcular o valor da Média
-        function notas(float $n1, float $n2){
-            $media = $n1 + $n2 / 2;
+        function notas(float $n1, float $n2):float{
+            $media = ($n1 + $n2) / 2;
             return $media;
         }
 
         // Chamando e guardando o retorno
-        $verificacao = notas(1.5, 6.5);
+        $verificacao = notas(10, 6.5);
 
 
         // Função 2: Calcular o valor da Média
@@ -43,10 +93,18 @@
         }
         
     ?>
-     <p>Média do Aluno: <?=$verificacao?></p>
-     <p>Situação do Aluno: <?=resultado($verificacao)?></p>
 
-    
+    <table>
+        <tr>
+            <th>Média</th>
+            <th>Situação</th>
+        </tr>
+
+        <tr>
+            <td><?=$verificacao?></td>
+            <td><?=resultado($verificacao)?></td>
+        </tr>
+    </table>
 
 
 
