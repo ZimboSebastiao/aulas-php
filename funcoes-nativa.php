8418dbc69ceb253770a38d9a548a7702be45970c
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-08-04 11:03:08 -0300

    Função: trim()

---
+++ funcoes-nativa.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funções nativas</title>
+</head>
+<body>
+    <h1>Exemplos de Funções nativas</h1>
+    <hr>
+
+    <h2>Strings</h2>
+    <h3>trim()</h3>
+    <?php 
+    // Função que remove espaços antes e depois de strings
+
+    $nome = "      Vinicius, e deve 80 paçocas       ";
+    $nomeSemEspaco = trim($nome)
+    ?>
+    <pre><?=var_dump($nome)?></pre>
+    <pre><?=var_dump($nomeSemEspaco)?></pre>
+
+
+
+
+
+
+
+
+
+    <h2>Númericas</h2>
+    <h2>Arrays</h2>
+    <h2>Strings</h2>
+    <h2>Strings</h2>
+    <h2>Strings</h2>
+    <h2>Strings</h2>
+    
+</body>
+</html>
