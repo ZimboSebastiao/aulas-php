30f1f25ccaf4aff3c7d47e76609f2dc418165381
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-08-09 22:55:54 -0300

    estilizando a pagina de cadastro

--- exercicios/exercicio-html07.php
+++ exercicios/exercicio-html07.php
@@ -5,59 +5,87 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercício 07</title>
+
+    <style>
+        * {box-sizing: border-box;} 
+
+        body {
+            background-color: #6a6679 !important;
+        }
+
+        .limitador {
+            width: 70vw;
+            margin: auto;
+        }
+
+        h1, #enviar  {text-align: center;}
+        
+    </style>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 </head>
 <body>
     <h1>Cadastro de Produtos</h1>
     <hr>
+    <main class="limitador">
+
+        <form action="exercicio07.php" method="post">
+    
+            <!-- ========== Nome ========== -->
+            <p class="input-group mb-3">
+                <!-- <label for="nome">Nome do Produto</label> -->
+                <span class="input-group-text" id="inputGroup-sizing-default">Nome do Produto</span>
+                <input type="text" name="nome" id="nome" require class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+            </p>
+    
+            <!-- ========== Fabricante ========== -->
+            <p class="form-floating">
+                <select name="fabricante" class="form-select" id="floatingSelect" aria-label="Floating label select example">
+                    <option selected></option>
+                    <?php foreach ($fabricantes as $fabricante) {?>
+                    <option value="<?=$fabricante;?>"><?=$fabricante?></option>
+                    <?php }?>
+                </select>    
+                <label  for="floatingSelect">Fabricante do Produto</label>
+            </p>
+    
+            <!-- ========== Preço ========== -->
+            <div class="input-group mb-3">
+                <span class="input-group-text">$</span>
+                <span class="input-group-text">Preço</span>
+                <input type="number" name="preco" id="preco" step=".01" min="100" max="10000" require class="form-control" aria-label="Dollar amount (with dot and two decimal places)">
+             </div>
+    
+            <!-- ========== Disponibilidade ========== -->
+            <div>
+                <p class="form-check">
+                    <input class="form-check-input" type="radio" name="disponibilidade" id="sim" value="Sim">
+                    <label class="form-check-label" for="sim">Disponível</label>
+                </p>
+                <p class="form-check">
+        
+                    <label class="form-check-label" for="nao">Indisponível</label>
+                    <input class="form-check-input" type="radio" name="disponibilidade" id="nao" value="Não">
+                </p>
+            </div>
+    
+            <!-- ========== Descrição ========== -->
+            <p class="form-floating">
+                <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px" name="descricao" cols="30" rows="10"></textarea>
+                <label for="floatingTextarea2">Descrição</label> <br>
+            </p>
+    
+            <!-- ========== Botão Enviar ========== -->
+            <p class="col-auto" id="enviar">
+                <button type="submit" name="enviar" id="enviar" class="btn btn-primary btn-lg">Enviar</button>
+            </p>
+        </form>
+
+    </main>
     
 
-    <form action="exercicio07.php" method="post">
-
-        <!-- ========== Nome ========== -->
-        <p>
-            <label for="nome">Nome do Produto</label>
-            <input type="text" name="nome" id="nome">
-        </p>
-
-        <!-- ========== Fabricante ========== -->
-        <p>
-            <label for="fabricante">Fabricante do Produto</label>
-            <select name="fabricante" id="fabricante">
-                <option value=""></option>
-                <?php foreach ($fabricantes as $fabricante) {?>
-                <option value="<?=$fabricante;?>"><?=$fabricante?></option>
-                <?php }?>
-            </select>    
-        </p>
-
-        <!-- ========== Preço ========== -->
-        <p>
-            <label for="preco">Preço</label>
-            <input type="number" name="preco" id="preco" step=".01" min="100" max="10000">
-        </p>
-
-        <!-- ========== Disponibilidade ========== -->
-        <p>
-            <label for="sim">Sim</label>
-            <input type="radio" name="disponibilidade" id="sim" value="Sim">
-
-            <label for="nao">Não</label>
-            <input type="radio" name="disponibilidade" id="nao" value="Não">
-        </p>
-
-        <!-- ========== Descrição ========== -->
-        <p>
-            <label for="descricao">Descrição</label> <br>
-            <textarea name="descricao" id="descricao" cols="30" rows="10"></textarea>
-        </p>
-
-        <!-- ========== Botão Enviar ========== -->
-        <p>
-            <button type="submit" name="enviar" id="enviar">Enviar</button>
-        </p>
-    </form>
 
 
-    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
 </html>
--- exercicios/exercicio07.php
+++ exercicios/exercicio07.php
@@ -1,13 +1,25 @@
 <?php
 
+    // ARRAY 
     $fabricantes = ["Samsung", "Lenovo", "Tecno", "Dell"];
 
+    // Sanatização
+    $nome = filter_input(INPUT_POST, "nome", FILTER_SANITIZE_SPECIAL_CHARS);
+    $fabricante = filter_input(INPUT_POST, "fabricante", FILTER_SANITIZE_SPECIAL_CHARS);
+    $preco = filter_input(INPUT_POST, 'preco', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+    $disponibilidade = filter_input(INPUT_POST, "disponibilidade", FILTER_SANITIZE_SPECIAL_CHARS);
+    $descricao = filter_input(INPUT_POST, "descricao", FILTER_SANITIZE_SPECIAL_CHARS);
 
-    if( !empty($_POST["nome"]) || !empty($_POST["preco"])){
+
+    // Condicional para verificar se o método é Post, e fazer a tratativa de erro
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        if( empty($_POST["nome"]) || empty($_POST["preco"])){
+        
 ?>
             
             <p style="color: red;">Voce deve preencher, os campos Nome e Preço.</p>
             <p><a href="exercicio-html07.php">Voltar</a></p>
 <?php
-    }       
+        }   
+    }     
 ?>
