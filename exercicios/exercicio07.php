02a3061beaa9d90d923d8939b3c40ddcd48614ab
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-08-10 09:56:04 -0300

    updates

--- exercicios/exercicio07.php
+++ exercicios/exercicio07.php
@@ -53,9 +53,29 @@
             }   else {
                 echo "dados cadastrado";
             }
-        }     
+        } 
+        
+        
     ?>
+    <table>
+        <tr>
+            <th></th>
+        </tr>
+
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+     <p>Nome do Produto: <?=$nome?></p>
+     <p>Fabricante do Produto: <?=$fabricante?></p>
+     <p>Preço do Produto<?=$preco?></p>
+     <p>Disponibilidade do Produto<?=$disponibilidade?></p>
+     <p>Descrição do Produto<?=$descricao?></p>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
 </html>
+
